a91ac41be22e159097a5fa36c0304093a9f249f4
lhohan <github@lhoest.eu>
2015-02-15 17:14:03 +0100

    Consistent function passing

--- scala/word-count/src/main/scala/word_count.scala
+++ scala/word-count/src/main/scala/word_count.scala
@@ -2,7 +2,7 @@ class Phrase(private val phrase: String) {
 
   def wordCount = {
     val words = phrase.split("[^\\w']").filter(_.nonEmpty)
-    val groupedWords = words.groupBy(s => s.toLowerCase)
+    val groupedWords = words.groupBy(_.toLowerCase)
     groupedWords.mapValues(_.size)
   }
 
