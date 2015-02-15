a7001cae88c886d19af609a8ec843e18bd11a43e
lhohan <github@lhoest.eu>
2015-02-15 21:48:54 +0100

    Rename variables

--- scala/etl/src/main/scala/etl.scala
+++ scala/etl/src/main/scala/etl.scala
@@ -1,9 +1,9 @@
 object ETL {
   def transform(in: Map[Int, Seq[String]]): Map[String, Int] = {
-    in.foldLeft(Map.empty[String, Int]){(acc, el) =>
-      val (i:Int, xs) = el
-      xs.foldLeft(acc){(acc1, el1) =>
-        acc1 + (el1.toLowerCase -> i)
+    in.foldLeft(Map.empty[String, Int]) { (stringsToValue, valueToStrings) =>
+      val (value, strings) = valueToStrings
+      strings.foldLeft(stringsToValue) { (stringsForValue, str) =>
+        stringsForValue + (str.toLowerCase -> value)
       }
     }
   }
