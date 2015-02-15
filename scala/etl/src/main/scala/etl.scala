16f64ebf735450fa299f420917c0d4a58f4136b1
lhohan <github@lhoest.eu>
2015-02-15 21:54:41 +0100

    using for-comprehension

--- scala/etl/src/main/scala/etl.scala
+++ scala/etl/src/main/scala/etl.scala
@@ -1,11 +1,6 @@
 object ETL {
-  def transform(in: Map[Int, Seq[String]]): Map[String, Int] = {
-    in.foldLeft(Map.empty[String, Int]) { (stringsToValue, valueToStrings) =>
-      val (value, strings) = valueToStrings
-      strings.foldLeft(stringsToValue) { (stringsForValue, str) =>
-        stringsForValue + (str.toLowerCase -> value)
-      }
-    }
-  }
-
+  def transform(in: Map[Int, Seq[String]]): Map[String, Int] = for {
+    (value, words) <- in
+    word <- words
+  } yield (word.toLowerCase, value)
 }
