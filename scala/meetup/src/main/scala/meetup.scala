ca9133946cd90a5440325f8c4e4fff505e656732
lhohan <github@lhoest.eu>
2015-02-16 20:50:26 +0100

    Meetup implemented.

--- scala/meetup/src/main/scala/meetup.scala
+++ scala/meetup/src/main/scala/meetup.scala
@@ -1,16 +1,54 @@
-import java.time.DayOfWeek
-import java.util.GregorianCalendar
-
-case class Meetup(m: Int, y: Int) {
-  def teenth(dayOfWeek: Meetup.DayOfWeek):GregorianCalendar = ???
-  def first(dayOfWeek: Meetup.DayOfWeek):GregorianCalendar = ???
-  def second(dayOfWeek: Meetup.DayOfWeek):GregorianCalendar = ???
-  def third(dayOfWeek: Meetup.DayOfWeek):GregorianCalendar = ???
-  def fourth(dayOfWeek: Meetup.DayOfWeek):GregorianCalendar = ???
-  def last(dayOfWeek: Meetup.DayOfWeek):GregorianCalendar = ???
-}
+import java.util.{Calendar, GregorianCalendar}
 
 object Meetup extends Enumeration {
   type DayOfWeek = Value
   val Mon, Tue, Wed, Thu, Fri, Sat, Sun = Value
 }
+
+case class Meetup(m: Int, y: Int) {
+
+  private def toJavaMonth(m: Int) = m - 1
+
+  private def days: Stream[GregorianCalendar] = daysFrom(new GregorianCalendar(y, toJavaMonth(m), 1)).takeWhile(c => c.get(Calendar.MONTH) == toJavaMonth(m))
+
+  private def daysFrom(day: GregorianCalendar): Stream[GregorianCalendar] = day #:: daysFrom(nextDay(day))
+
+  private def nextDay(calendar: Calendar) = {
+    val next = new GregorianCalendar()
+    next.setTime(calendar.getTime)
+    next.add(Calendar.DAY_OF_MONTH, 1)
+    next
+  }
+
+  private def daysOfWeekday(dow: Int): Stream[GregorianCalendar] = days.filter(_.get(Calendar.DAY_OF_WEEK) == dow)
+
+  private def daysOfWeekday(dow: Meetup.DayOfWeek): Stream[GregorianCalendar] = daysOfWeekday(convert(dow))
+
+  private def convert(dow: Meetup.DayOfWeek): Int = dow match {
+    case Meetup.Mon => Calendar.MONDAY
+    case Meetup.Tue => Calendar.TUESDAY
+    case Meetup.Wed => Calendar.WEDNESDAY
+    case Meetup.Thu => Calendar.THURSDAY
+    case Meetup.Fri => Calendar.FRIDAY
+    case Meetup.Sat => Calendar.SATURDAY
+    case Meetup.Sun => Calendar.SUNDAY
+  }
+
+  private def ith(dayOfWeek: Meetup.DayOfWeek, i: Int) = daysOfWeekday(dayOfWeek).drop(i - 1).head
+
+  def teenth(dayOfWeek: Meetup.DayOfWeek): GregorianCalendar = {
+    daysOfWeekday(dayOfWeek).filter(_.get(Calendar.DAY_OF_MONTH) / 10 == 1).last
+  }
+
+  def first(dayOfWeek: Meetup.DayOfWeek): GregorianCalendar = ith(dayOfWeek, 1)
+
+  def second(dayOfWeek: Meetup.DayOfWeek): GregorianCalendar = ith(dayOfWeek, 2)
+
+  def third(dayOfWeek: Meetup.DayOfWeek): GregorianCalendar = ith(dayOfWeek, 3)
+
+  def fourth(dayOfWeek: Meetup.DayOfWeek): GregorianCalendar = ith(dayOfWeek, 4)
+
+  def last(dayOfWeek: Meetup.DayOfWeek): GregorianCalendar = daysOfWeekday(dayOfWeek).last
+
+}
+
