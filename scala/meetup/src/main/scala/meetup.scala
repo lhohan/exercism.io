67e6f18a254533173fedcc2ce1056c1ea0495f65
lhohan <github@lhoest.eu>
2015-02-16 22:20:16 +0100

    Meetup implemented - shorter again

--- scala/meetup/src/main/scala/meetup.scala
+++ scala/meetup/src/main/scala/meetup.scala
@@ -1,36 +1,35 @@
+import java.util.Calendar._
 import java.util.{Calendar, GregorianCalendar}
 
 object Meetup {
-  val Mon = Calendar.MONDAY
-  val Tue = Calendar.TUESDAY
-  val Wed = Calendar.WEDNESDAY
-  val Thu = Calendar.THURSDAY
-  val Fri = Calendar.FRIDAY
-  val Sat = Calendar.SATURDAY
-  val Sun = Calendar.SUNDAY
+  val Mon = MONDAY
+  val Tue = TUESDAY
+  val Wed = WEDNESDAY
+  val Thu = THURSDAY
+  val Fri = FRIDAY
+  val Sat = SATURDAY
+  val Sun = SUNDAY
 }
 
 case class Meetup(m: Int, y: Int) {
 
-  private def toJavaMonth(m: Int) = m - 1
-
-  private def days: Stream[GregorianCalendar] = daysFrom(new GregorianCalendar(y, toJavaMonth(m), 1)).takeWhile(c => c.get(Calendar.MONTH) == toJavaMonth(m))
+  private val jMonth = m - 1
 
   private def daysFrom(day: GregorianCalendar): Stream[GregorianCalendar] = day #:: daysFrom(nextDay(day))
 
+  private def days: Stream[GregorianCalendar] = daysFrom(new GregorianCalendar(y, jMonth, 1)).takeWhile(_.get(MONTH) == jMonth)
+
+
   private def nextDay(calendar: Calendar) = {
-    val next = new GregorianCalendar()
-    next.setTime(calendar.getTime)
-    next.add(Calendar.DAY_OF_MONTH, 1)
-    next
+    new GregorianCalendar(calendar.get(YEAR), calendar.get(MONTH), calendar.get(DATE) + 1)
   }
 
-  private def daysOfWeekday(dow: Int): Stream[GregorianCalendar] = days.filter(_.get(Calendar.DAY_OF_WEEK) == dow)
+  private def daysOfWeekday(weekDay: Int): Stream[GregorianCalendar] = days.filter(_.get(DAY_OF_WEEK) == weekDay)
 
-  private def ith(dayOfWeek: Int, i: Int) = daysOfWeekday(dayOfWeek).drop(i - 1).head
+  private def ith(weekDay: Int, i: Int) = daysOfWeekday(weekDay).drop(i - 1).head
 
   def teenth(dayOfWeek: Int): GregorianCalendar = {
-    daysOfWeekday(dayOfWeek).filter(_.get(Calendar.DAY_OF_MONTH) / 10 == 1).last
+    daysOfWeekday(dayOfWeek).filter(_.get(DAY_OF_MONTH) / 10 == 1).last
   }
 
   def first(dayOfWeek: Int): GregorianCalendar = ith(dayOfWeek, 1)
