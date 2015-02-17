97352c660b2fc1c86eb2d0b4e5b68f34b906eb2f
lhohan <github@lhoest.eu>
2015-02-17 21:41:08 +0100

    Space age implementation

---
+++ scala/space-age/src/main/scala/space_age.scala
@@ -0,0 +1,21 @@
+case class SpaceAge(age: Long) {
+
+  def onNeptune: Long = ???
+
+  def seconds: Long = age
+
+  def onUranus: Long = ???
+
+  def onSaturn: Long = ???
+
+  def onJupiter: Long = ???
+
+  def onMars: Long = ???
+
+  def onVenus: Long = ???
+
+  def onMercury: Long = ???
+
+  def onEarth: Long = ???
+
+}
