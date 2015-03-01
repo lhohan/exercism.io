443ee148dcea5b1a48c435eae4f722cc82fa1a2c
lhohan <github@lhoest.eu>
2015-03-01 15:27:20 +0100

    Accumulate solution without map as in asked in assignment.

--- scala/accumulate/src/main/scala/accumulate.scala
+++ scala/accumulate/src/main/scala/accumulate.scala
@@ -1,3 +1,6 @@
 class Accumulate {
-  def accumulate[A, B](f: A => B, xs: List[A]) = xs.map(f)
+  def accumulate[A, B](f: A => B, as: List[A]): List[B] = as match {
+    case Nil => Nil
+    case x :: xs => f(x) :: accumulate(f, xs)
+  }
 }
