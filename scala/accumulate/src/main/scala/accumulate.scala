416b9cf903473b65180953df690ce64f31d5b3fc
lhohan <github@lhoest.eu>
2015-03-01 16:15:02 +0100

    Accumulate solution : tail recursively.

--- scala/accumulate/src/main/scala/accumulate.scala
+++ scala/accumulate/src/main/scala/accumulate.scala
@@ -1,6 +1,21 @@
+import scala.annotation.tailrec
+
 class Accumulate {
-  def accumulate[A, B](f: A => B, as: List[A]): List[B] = as match {
-    case Nil => Nil
-    case x :: xs => f(x) :: accumulate(f, xs)
+  def accumulate[A, B](f: A => B, as: List[A]): List[B] = {
+    @tailrec
+    def loop(ass: List[A], acc: List[B]): List[B] = ass match {
+      case Nil => acc
+      case x :: xs => loop(xs, acc :+ f(x))
+    }
+    loop(as, Nil)
   }
 }
+
+// Non-tail recursive solution
+
+//class Accumulate {
+//  def accumulate[A, B](f: A => B, as: List[A]): List[B] = as match {
+//    case Nil => Nil
+//    case x :: xs => f(x) :: accumulate(f, xs)
+//  }
+//}
