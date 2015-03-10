a5d84d7db9049fc5f967d237cdf5ed1ef349d430
lhohan <github@lhoest.eu>
2015-03-10 21:42:44 +0100

    sublist - A solution without method from the standard library

--- scala/sublist/src/main/scala/sublist.scala
+++ scala/sublist/src/main/scala/sublist.scala
@@ -1,3 +1,5 @@
+import scala.annotation.tailrec
+
 class Sublist {
 
   def sublist[A](xs: List[A], ys: List[A]): Sublist.Value =
@@ -8,8 +10,24 @@ class Sublist {
       case (false, false) => Sublist.Unequal
     }
 
-  private def isSuperlist[A](xs: List[A], ys: List[A]) = xs.containsSlice(ys)
+  @tailrec
+  private def isSuperlist[A](xs: List[A], ys: List[A]): Boolean =
+    if (startsWith(xs, ys)) {
+      true
+    } else if (xs.size <= ys.size) {
+      false
+    } else {
+      isSuperlist(xs.tail, ys)
+    }
 
+  /** xs startsWith ys */
+  def startsWith[A](xs: List[A], ys: List[A]): Boolean =
+    (xs, ys) match {
+      case (_, Nil)                       => true
+      case (Nil, _)                       => false
+      case (x :: xss, y :: yss) if x == y => startsWith(xss, yss)
+      case _                              => false
+    }
 }
 
 object Sublist extends Enumeration {
