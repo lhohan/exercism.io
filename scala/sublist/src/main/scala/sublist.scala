db50af72566e8b1a10f6de5580f98efe5315e1d4
lhohan <github@lhoest.eu>
2015-03-10 21:27:49 +0100

    sublist - rename helper method to be correct

--- scala/sublist/src/main/scala/sublist.scala
+++ scala/sublist/src/main/scala/sublist.scala
@@ -1,14 +1,14 @@
 class Sublist {
 
   def sublist[A](xs: List[A], ys: List[A]): Sublist.Value =
-    (isSublist(xs, ys), isSublist(ys, xs)) match {
+    (isSuperlist(xs, ys), isSuperlist(ys, xs)) match {
       case (true, true)   => Sublist.Equal
       case (true, false)  => Sublist.Superlist
       case (false, true)  => Sublist.Sublist
       case (false, false) => Sublist.Unequal
     }
 
-  private def isSublist[A](xs: List[A], ys: List[A]) = xs.containsSlice(ys)
+  private def isSuperlist[A](xs: List[A], ys: List[A]) = xs.containsSlice(ys)
 
 }
 
