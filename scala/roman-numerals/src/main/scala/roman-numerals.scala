49a554258f591afb0b0e770d408d7404d6b165a4
lhohan <github@lhoest.eu>
2015-03-18 21:12:28 +0100

    Roman numerals - cleanup

--- scala/roman-numerals/src/main/scala/roman-numerals.scala
+++ scala/roman-numerals/src/main/scala/roman-numerals.scala
@@ -8,63 +8,66 @@ case class RomanNumeral(r: Int) {
 
   case class RomanRange(lower: DigitRoman, upper: DigitRoman, lowerOfLower: Option[DigitRoman] = None)
 
-  //  @tailrec
   def convert(x: Int, range: RomanRange): String = {
     @tailrec
-    def expandedRoman(x: Int, acc: String, digits: List[Int]): String = {
-      (x, digits) match {
-        case (0, _)                                        => acc
-        case (_, Nil)                                      => throw new RuntimeException("no more digits")
-        case (xx, d1 :: d2 :: d3 :: ds) if (d1 - d3) == xx => expandedRoman(xx - (d1 - d3), acc + s"${digitToRoman(d3)}${digitToRoman(d1)}", ds)
-        case (xx, d1 :: d2 :: ds) if (d1 - d2) == xx && d2 != xx => expandedRoman(xx - (d1 - d2), acc + s"${digitToRoman(d2)}${digitToRoman(d1)}", ds)
-        case (xx, d :: ds) if (xx - d) < 0                 => expandedRoman(xx, acc, ds)
-        case (xx, d :: ds) if (xx - d) == 0                => expandedRoman(xx - d, acc + digitToRoman(d), ds)
-        case (xx, d :: ds) if (xx - d) > 0                 => expandedRoman(xx - d, acc + digitToRoman(d), d :: ds)
+    def convertLoop(remaining: Int, acc: String, nums: List[Int]): String =
+      (remaining, nums) match {
+        case (0, _)                                      =>
+          acc
+        case (_, Nil)                                    =>
+          throw new RuntimeException("no more numbers")
+        case (r, n1 :: n2 :: n3 :: ns) if (n1 - n3) == r =>
+          convertLoop(r - (n1 - n3), acc + s"${digiToRoman(n3)}${digiToRoman(n1)}", ns)
+        case (r, n1 :: n2 :: ns) if n2 == r              =>
+          convertLoop(0, acc + s"${digiToRoman(r)}", ns)
+        case (r, n1 :: n2 :: ns) if (n1 - n2) == r       =>
+          convertLoop(r - (n1 - n2), acc + s"${digiToRoman(n2)}${digiToRoman(n1)}", ns)
+        case (r, n :: ns) if (r - n) < 0                 =>
+          convertLoop(r, acc, ns)
+        case (r, n :: ns) if (r - n) == 0                =>
+          convertLoop(r - n, acc + digiToRoman(n), ns)
+        case (r, n :: ns) if (r - n) > 0                 =>
+          convertLoop(r - n, acc + digiToRoman(n), n :: ns)
       }
-    }
-    expandedRoman(x, "", digitsDesc)
-  }
-
-
-  private def digits(int: Int) = {
-    @tailrec
-    def pow(x: Int, y: Int, t: Int = 1): Int = if (y == 0) {
-      t
-    } else {
-      pow(x, y - 1, t * x)
-    }
-    val chars = int.toString.toList
-    val tuples = chars.zip((0 until chars.length).reverse)
-    tuples.map { case (c, d) => c.asDigit * pow(10, d)}
-  }
-
-  @tailrec
-  private def findRange(x: Int, romans: List[(Int, Char)], boundaries: RomanRange): RomanRange = {
-    val upper = boundaries.upper
-    if (x > 1000) {
-      RomanRange((Int.MaxValue, '-'), romans.head, Some(romans.drop(1).head))
-    } else
-    if (x <= upper._1) {
-      boundaries
-    } else {
-      findRange(x, romans.tail, RomanRange(upper, romans.head, Some(boundaries.lower)))
-    }
+    convertLoop(x, "", digiDesc)
   }
 
   private def toRoman: Int => String = x => {
-    val range = findRange(x, dtrSorted, RomanRange(dtrSorted.head, dtrSorted.drop(1).head))
 
-    convert(x, range)
+    @tailrec
+    def findRange(x: Int, romans: List[DigitRoman], boundaries: RomanRange): RomanRange = {
+      val upper = boundaries.upper
+      if (x > 1000) {
+        RomanRange((Int.MaxValue, '-'), romans.head, Some(romans.drop(1).head))
+      } else
+      if (x <= upper._1) {
+        boundaries
+      } else {
+        findRange(x, romans.tail, RomanRange(upper, romans.head, Some(boundaries.lower)))
+      }
+    }
+
+    convert(x, findRange(x, digiToRomanSorted, RomanRange(digiToRomanSorted.head, digiToRomanSorted.drop(1).head)))
   }
 
   val value = {
+    // 576 -> (500, 70, 6)
+    def digits(int: Int) = {
+      @tailrec
+      def pow(x: Int, y: Int, t: Int = 1): Int = if (y == 0) {
+        t
+      } else {
+        pow(x, y - 1, t * x)
+      }
+      val digits = int.toString.toList
+      digits.zip((0 until digits.length).reverse).map { case (c, d) => c.asDigit * pow(10, d)}
+    }
+
     if (r == 0) {
       ""
     }
     else {
-      val result = digits(r).map(toRoman)
-      println(result)
-      result.mkString
+      digits(r).map(toRoman).mkString
     }
 
   }
@@ -72,12 +75,12 @@ case class RomanNumeral(r: Int) {
 }
 
 object RomanNumeral {
-  val romanToDigit = Map(
+  val romanToDigi = Map(
     'I' -> 1, 'V' -> 5, 'X' -> 10,
     'L' -> 50, 'C' -> 100, 'D' -> 500, 'M' -> 1000
   )
-  val digitToRoman = romanToDigit.map(_.swap)
+  val digiToRoman = romanToDigi.map(_.swap)
   // List((1,I), (5,V), (10,X), (50,L), (100,C), (500,D), (1000,M))
-  val dtrSorted = digitToRoman.toList.sorted
-  val digitsDesc = dtrSorted.map(_._1).reverse
+  val digiToRomanSorted = digiToRoman.toList.sorted
+  val digiDesc = digiToRomanSorted.map(_._1).reverse
 }
