f61c0d7e7ca580d81f5fdcb47b2424365abb596f
lhohan <github@lhoest.eu>
2015-03-24 22:13:01 +0100

    PrimeFactors: first solution. Cleanup.

--- scala/prime-factors/src/main/scala/prime-factors.scala
+++ scala/prime-factors/src/main/scala/prime-factors.scala
@@ -6,24 +6,17 @@ object PrimeFactors {
 
   def primeFactors(number: Long): List[Long] = {
 
-    def from(long: Long): Stream[Long] = Stream.cons(long, from(long + 1))
+    def from(long: Long): Stream[Long] = long #:: from(long + 1)
 
     @tailrec
-    def factors(n: Long, acc: List[Long], ps: Stream[Long], currentPrime: Long = 2): List[Long] = {
-      if (n == 1) {
-        acc
-      } else {
-        val remainder = n % currentPrime
-        if (remainder == 0) {
-          val quotient = n / currentPrime
-          factors(quotient, acc :+ currentPrime, ps, currentPrime)
-        } else {
-          factors(n, acc, ps.tail, ps.head)
-        }
+    def factors(n: Long, acc: List[Long], ps: Stream[Long], currentPrime: Long): List[Long] =
+      (n, n % currentPrime) match {
+        case (1, _) => acc
+        case (_, 0) => factors(n / currentPrime, acc :+ currentPrime, ps, currentPrime)
+        case _      => factors(n, acc, ps.tail, ps.head)
       }
-    }
 
     lazy val primes: Stream[Long] = 2 #:: from(3).filter(_ % primes.head != 0)
-    factors(number, List.empty[Long], primes)
+    factors(number, List.empty[Long], primes.tail, primes.head)
   }
 }
