c72413035efc12d91e1518e42c08410cf769a4d4
lhohan <github@lhoest.eu>
2015-03-29 15:04:49 +0200

    raindrop solution 2

--- scala/raindrops/src/main/scala/raindrops.scala
+++ scala/raindrops/src/main/scala/raindrops.scala
@@ -1,12 +1,9 @@
 object Raindrops {
   def apply() = this
-
   def raindrops = (3, "Pling") :: (5, "Plang") :: (7, "Plong") :: Nil
 
   def convert(n: Int) = {
-    val converted = raindrops.foldLeft("")((acc, raindrop) =>
-      if (n % raindrop._1 == 0) acc + raindrop._2 else acc
-    )
-    if (converted.isEmpty) n.toString else converted
+    val sounds = raindrops.map(x => if (n % x._1 == 0) x._2 else "").mkString
+    if (sounds.nonEmpty) sounds else n.toString
   }
 }
