c1a5a5a332a36e2856080c727a3f3faedcae453d
lhohan <github@lhoest.eu>
2015-09-30 20:06:57 +0200

    Map is also a function

--- scala/atbash-cipher/src/main/scala/Atbash.scala
+++ scala/atbash-cipher/src/main/scala/Atbash.scala
@@ -1,6 +1,11 @@
-/**
- * Created by hans on 30/09/15.
- */
 object Atbash {
+  def apply() = this
 
+  val letters = 'a' to 'z'
+  val digits = (0 to 9).map(d => ('0' + d).toChar)
+  val encoder = letters.zip(letters.reverse).toMap ++ digits.zip(digits).toMap
+
+  def encode(s: String): String = {
+    s.toLowerCase.filter(encoder.keySet.contains).map(encoder).grouped(5).mkString(" ")
+  }
 }
