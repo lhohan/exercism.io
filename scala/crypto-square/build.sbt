791522eddfb5bdaa1ef60485c3d76ec7cac07573
lhohan <github@lhoest.eu>
2015-10-02 19:55:10 +0200

    Exercism cryptosquare: init modeule and first working version. Needs refactoring.

---
+++ scala/crypto-square/build.sbt
@@ -0,0 +1,5 @@
+scalaVersion := "2.10.3"
+
+libraryDependencies += "org.scalatest" % "scalatest_2.10" % "2.0" % "test"
+
+testOptions in Test += Tests.Argument("-oD")
---
+++ scala/crypto-square/src/main/scala/CryptoSquare.scala
@@ -0,0 +1,51 @@
+object CryptoSquare {
+
+  def apply() = this
+
+  def normalizedCiphertext(s: String): String = transpose(s).mkString(" ")
+
+  def ciphertext(s: String): String = transpose(s).mkString("")
+
+  private def transpose(s: String): List[String] = {
+    def transpose(m: List[String], acc: List[String]): List[String] = {
+      if (m.flatten.isEmpty) {
+        acc
+      } else {
+        val firstColToRow = m.foldLeft(List.empty[Char]) {
+          (acc, c) =>
+            c.toCharArray.toList match {
+              case Nil               => acc
+              case (h: Char) :: rest => h :: acc
+            }
+        }.reverse
+        val withoutFirstCol = m.foldLeft(List.empty[String]) {
+          (acc, c) => c.toCharArray.toList match {
+            case Nil               => acc
+            case (_: Char) :: rest => rest.mkString("") :: acc
+          }
+        }.reverse
+        transpose(withoutFirstCol, firstColToRow.mkString("") :: acc)
+      }
+    }
+    transpose(plaintextSegments(s), Nil).reverse
+  }
+
+  def plaintextSegments(s: String): List[String] = {
+    val normalized = normalizePlaintext(s)
+    val size = squareSize(normalized)
+
+    if (size == 0) {
+      List("")
+    } else {
+      normalized.grouped(size).toList
+    }
+  }
+
+  def squareSize(s: String): Int = squares.dropWhile { case (i, x) => x < s.length }.head._1
+
+  def normalizePlaintext(s: String): String = s.filter(_.isLetterOrDigit).toLowerCase
+
+  private val squares: Stream[(Int, Int)] = Stream.from(0).map { x => (x, x * x) }
+
+
+}
