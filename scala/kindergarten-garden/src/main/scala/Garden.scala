25dcc0f2461b82687694d1ba8c32012ab528c2f0
lhohan <github@lhoest.eu>
2015-10-04 11:49:32 +0200

    Exercism KinderGarten: same implementation but made more clear using types, using intermediate vales and less method chaining.

--- scala/kindergarten-garden/src/main/scala/Garden.scala
+++ scala/kindergarten-garden/src/main/scala/Garden.scala
@@ -1,33 +1,57 @@
+import scala.annotation.tailrec
 
 object Garden {
-  val default = List(
-    "Alice", "Bob", "Charlie", "David",
-    "Eve", "Fred", "Ginny", "Harriet",
-    "Ileana", "Joseph", "Kincaid", "Larry"
+
+  type Child = String
+
+  private val defaultChildren = List("Alice", "Bob", "Charlie", "David", "Eve", "Fred", "Ginny", "Harriet", "Ileana",
+    "Joseph", "Kincaid", "Larry"
   )
 
-  def defaultGarden(garden: String): Garden = Garden(default, garden)
+  def defaultGarden(garden: String): Garden = Garden(defaultChildren, garden)
 }
 
-case class Garden(private val _names: List[String], private val _garden: String) {
+import Garden.Child
+
+case class Garden(private val _childeren: List[Child], private val _garden: String) {
 
   import Plant.Plant
 
-  type Row = List[String]
-
-  private val names = _names.sorted
-  private val namedGarden: List[Row] = { // first row has names
-    names +: _garden.split( """\n""").map(row => row.grouped(2).toList).toList
+  case class Window(p1: Plant, p2: Plant) {
+    val plants = List(p1, p2)
   }
 
-  def getPlants(name: String): List[Plant] = {
-    def find(garden: List[Row]): List[Plant] = garden match {
-      case List(n :: ns, p1 :: row1, p2 :: row2) if n == name => (p1 + p2).map(c => Plant.withName(c.toString)).toList
-      case List(_ :: ns, _ :: row1, _ :: row2)                => find(List(ns, row1, row2))
-      case List(_, Nil, Nil)                                  => List.empty[Plant]
+  case class GardenRows(r1: GardenRow, r2: GardenRow)
 
+  type Children = List[Child]
+  type GardenRow = List[Window]
+
+  implicit def charToPlant(c: Char): Plant = Plant.withName(c.toString)
+
+  private val children: Children = _childeren.sorted
+  private val garden: GardenRows = {
+    val gardenRows = _garden.split( """\n""").toList
+    val gardenWindows = gardenRows.map { row =>
+      val pairs = row.grouped(2).toList
+      val windows = pairs.map { pair => Window(pair.head, pair.last) }
+      windows
     }
-    find(namedGarden)
+    GardenRows(gardenWindows.head, gardenWindows.last)
+  }
+
+  def getPlants(child: Child): List[Plant] = {
+    @tailrec
+    def find(garden: GardenRows, children: Children): List[Plant] = {
+      val (row1, row2) = (garden.r1, garden.r2)
+      children match {
+        case c :: cs if c == child => row1.head.plants ++ row2.head.plants
+        case _ :: cs               => (row1, row2) match {
+          case (_ :: nextRow1, _ :: nextRow2) => find(GardenRows(nextRow1, nextRow2), cs)
+          case _                              => List.empty[Plant]
+        }
+      }
+    }
+    find(garden, children)
   }
 }
 
