e565927d23a2a6f4ad9fbded0a17b8a714618e3c
lhohan <github@lhoest.eu>
2015-10-05 20:50:18 +0200

    Exercism: More readable version of Atbash.

--- scala/atbash-cipher/src/main/scala/Atbash.scala
+++ scala/atbash-cipher/src/main/scala/Atbash.scala
@@ -1,11 +1,21 @@
 object Atbash {
   def apply() = this
 
-  val letters = 'a' to 'z'
-  val digits = (0 to 9).map(d => ('0' + d).toChar)
-  val encoder = letters.zip(letters.reverse).toMap ++ digits.zip(digits).toMap
+
+  private val atbash = {
+    val letters = 'a' to 'z'
+    val digits = (0 to 9).map(d => ('0' + d).toChar)
+    val atbashLetterMapping = letters.zip(letters.reverse)
+    val digitMapping: IndexedSeq[(Char, Char)] = digits.zip(digits)
+
+    (atbashLetterMapping ++ digitMapping).toMap
+  }
 
   def encode(s: String): String = {
-    s.toLowerCase.filter(encoder.keySet.contains).map(encoder).grouped(5).mkString(" ")
+    def isSupportedChar: (Char) => Boolean = atbash.keySet.contains
+    def format(a: String): String = a.grouped(5).mkString(" ")
+
+    val encoded = s.toLowerCase.filter(isSupportedChar).map(atbash)
+    format(encoded)
   }
 }
